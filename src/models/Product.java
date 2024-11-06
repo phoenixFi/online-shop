e009a8f17d7a2cde3da24ce3cb84dbb99428903a
madiyarofficalgmail.com <erbatyrov.madiyar@gmail.com>
2024-11-06 22:13:11 +0500

    add product

---
+++ src/models/Product.java
@@ -0,0 +1,26 @@
+package models;
+
+public class Product {
+    private int productId;
+    private String name;
+    private double price;
+
+    public Product(int productId, String name, double price) {
+        this.productId = productId;
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
+
