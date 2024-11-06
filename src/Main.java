09cc7493fb34dd35d5cd44dbde544e5ed2413cc9
tokhtar <toktari005@gmail.com>
2024-11-06 23:02:48 +0500

    added Main

--- src/Main.java
+++ src/Main.java
@@ -4,26 +4,21 @@ import views.StoreView;
 
 public class Main {
     public static void main(String[] args) {
-        // Создание продуктов
         Product product1 = new Product(1, "Laptop", 1000);
         Product product2 = new Product(2, "Smartphone", 500);
 
-        // Создание и настройка MVC-компонентов
         StoreView view = new StoreView();
         Order order = new Order();
         OrderController controller = new OrderController(view, order);
 
-        // Добавление продуктов в заказ
         controller.addProductToOrder(product1);
         controller.addProductToOrder(product2);
 
-        // Создание фасада для обработки заказа
         OrderProcessingFacade facade = new OrderProcessingFacade();
         facade.addProduct(product1);
         facade.addProduct(product2);
         facade.processOrder("paypal");
 
-        // Применение наблюдателя
         OrderObserver observer = new OrderObserver() {
             @Override
             public void update(String status) {
